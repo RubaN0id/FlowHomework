5570070994ca3e7c5909c607d41f7fb391da9521
Kirill Ruban <krumsosh@gmail.com>
2025-02-26 22:51:53 +0300

    Flow homework

--- flowcats/src/main/java/otus/homework/flowcats/CatsViewModel.kt
+++ flowcats/src/main/java/otus/homework/flowcats/CatsViewModel.kt
@@ -2,30 +2,37 @@ package otus.homework.flowcats
 
 import androidx.lifecycle.*
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
 class CatsViewModel(
-    private val catsRepository: CatsRepository
+    catsRepository: CatsRepository
 ) : ViewModel() {
 
-    private val _catsLiveData = MutableLiveData<Fact>()
-    val catsLiveData: LiveData<Fact> = _catsLiveData
+    private val _catsFlow = MutableStateFlow<Result>(Result.Progress)
+    val catsFlow = _catsFlow.asStateFlow()
 
     init {
-        viewModelScope.launch {
-            withContext(Dispatchers.IO) {
-                catsRepository.listenForCatFacts().collect {
-                    _catsLiveData.value = it
-                }
-            }
-        }
+        catsRepository.listenForCatFacts()
+            .onEach { _catsFlow.value = Result.Success(it) }
+            .catch { _catsFlow.value = Result.Error(it.message) }
+            .launchIn(viewModelScope)
     }
 }
 
-class CatsViewModelFactory(private val catsRepository: CatsRepository) :
-    ViewModelProvider.NewInstanceFactory() {
-    override fun <T : ViewModel?> create(modelClass: Class<T>): T =
-        CatsViewModel(catsRepository) as T
+class CatsViewModelFactory(private val catsRepository: CatsRepository) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T =
+        modelClass.getConstructor(CatsRepository::class.java).newInstance(catsRepository)
+}
+
+sealed class Result {
+    data object Progress : Result()
+    data class Success(val fact: Fact) : Result()
+    data class Error(val message: String?) : Result()
 }
